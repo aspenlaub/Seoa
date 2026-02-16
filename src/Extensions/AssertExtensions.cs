da644ceda081e6614ce0afd91d41fb0baa452b1f
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2026-02-16 22:44:14 +0100

    Start adding a bit of substance

---
+++ src/Extensions/AssertExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Aspenlaub.Net.GitHub.CSharp.Pegh.Extensions;
+using Aspenlaub.Net.GitHub.CSharp.Pegh.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aspenlaub.Net.GitHub.CSharp.Seoa.Extensions;
+
+public static class AssertExtensions {
+    public static void ThereWereNoErrors(this Assert assert, IErrorsAndInfos errorsAndInfos) {
+        if (!errorsAndInfos.AnyErrors()) {
+            return;
+        }
+
+        throw new AssertFailedException(errorsAndInfos.ErrorsToString());
+    }
+}
--- src/Test/SeoaTest.cs
+++ src/Test/SeoaTest.cs
@@ -1,4 +1,6 @@
-﻿using Aspenlaub.Net.GitHub.CSharp.Seoa.Components;
+﻿using Aspenlaub.Net.GitHub.CSharp.Pegh.Entities;
+using Aspenlaub.Net.GitHub.CSharp.Seoa.Components;
+using Aspenlaub.Net.GitHub.CSharp.Seoa.Extensions;
 using Autofac;
 
 namespace Aspenlaub.Net.GitHub.CSharp.Seoa.Test;
@@ -9,5 +11,12 @@ public sealed class SeoaTest {
     public void CanBuildSeoaContainer() {
         IContainer container = new ContainerBuilder().UseSeoa(nameof(SeoaTest)).Build();
         Assert.IsNotNull(container);
+    }
+
+    [TestMethod]
+    public void NoErrors() {
+        var errorsAndInfos = new ErrorsAndInfos();
+        errorsAndInfos.Errors.Add("This is not an error");
+        Assert.That.ThereWereNoErrors(errorsAndInfos);
     }
 }
