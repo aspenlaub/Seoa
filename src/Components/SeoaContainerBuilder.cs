813f6b7da3a3f8d43d03549945f27682bdf73fec
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2026-02-16 22:31:25 +0100

    Add some contents

---
+++ src/Components/SeoaContainerBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Aspenlaub.Net.GitHub.CSharp.Seoa.Components;
+
+public static class SeoaContainerBuilder {
+    public static ContainerBuilder UseSeoa(this ContainerBuilder builder, string applicationName) {
+        return builder;
+    }
+
+    public static IServiceCollection UseSeoa(this IServiceCollection services, string applicationName) {
+        return services;
+    }
+}
---
+++ src/Test/SeoaTest.cs
@@ -0,0 +1,13 @@
+﻿using Aspenlaub.Net.GitHub.CSharp.Seoa.Components;
+using Autofac;
+
+namespace Aspenlaub.Net.GitHub.CSharp.Seoa;
+
+[TestClass]
+public sealed class SeoaTest {
+    [TestMethod]
+    public void CanBuildSeoaContainer() {
+        IContainer container = new ContainerBuilder().UseSeoa(nameof(SeoaTest)).Build();
+        Assert.IsNotNull(container);
+    }
+}
